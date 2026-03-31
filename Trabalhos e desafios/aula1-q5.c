243ae6e1629ac537a62cd4e2f1c60439a7fc1e43
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2026-03-31 09:09:03 -0300

    Add files via upload

--- Trabalhos e desafios/aula1-q5.c
+++ Trabalhos e desafios/aula1-q5.c
@@ -32,31 +32,46 @@ void* task(void *arg);
 void showArray(int *v, int size);
 
 int main(int argc, char *argv[]){
-	if(argc != 3)
+	if(argc != 3){
+		printf("Invalid arguments\n");		
 		exit(EXIT_FAILURE);
+	}
 	
 	int n_threads = atoi(argv[1]); // M (nº threads)
 	arraySize = atoi(argv[2]);     // N (elementos vetor)
 	
-	thread = (pthread_t *) malloc(n_threads*sizeof(pthread_t));
-	thread_params = (t_params *) malloc(n_threads*sizeof(t_params));
-	
-	v = (int *) calloc(arraySize, sizeof(int));
+	if((thread = (pthread_t *) malloc(n_threads*sizeof(pthread_t))) == NULL)
+		exit(2);
+	if((thread_params = (t_params *) malloc(n_threads*sizeof(t_params))) == NULL)
+		exit(3);
+	if((v = (int *) calloc(arraySize, sizeof(int))) == NULL)
+		exit(4);
 	
 	int workSize = arraySize / n_threads;   // parcela de cada thread
 	int remainder = arraySize % n_threads;  // restante a ser dividido pelas threads
 	
-	for(int i = 0; i < n_threads; ++i){
-		thread_params[i].start = workSize*i;
-		thread_params[i].finish = workSize*(i+1)-1;
+	if(remainder == 0){
+		for(int i = 0; i < n_threads; ++i){
+			thread_params[i].start = workSize*i;
+			thread_params[i].finish = workSize*(i+1)-1;
+			thread_params[i].remainingIndexOffset = -1;		
+		}
+	}
+	else{
+		for(int i = 0; i < n_threads; ++i){
+			thread_params[i].start = workSize*i;
+			thread_params[i].finish = workSize*(i+1)-1;
 		
-		if(remainder--) // se ainda tem resto, coloca o offset
+		if(remainder-- > 0) // se ainda tem resto, dá o offset dos elem. restantes p/ as threads finalizarem
 			thread_params[i].remainingIndexOffset = i;
 		else
 			thread_params[i].remainingIndexOffset = -1;
+		}
 	}
-	
-	// criacao das threads
+		
+	// clock_t start = clock();
+
+	// criacao das threads	
 	for(int i = 0; i < n_threads; ++i)
 		pthread_create(&thread[i], NULL, (void *) task, (void*) &thread_params[i]);
 	
@@ -64,15 +79,18 @@ int main(int argc, char *argv[]){
 	for(int i = 0; i < n_threads; ++i)
 		pthread_join(thread[i], NULL);
 	
+	// clock_t end = clock();
+	// double diff = ((double) (end-start) / CLOCKS_PER_SEC);
+	// printf("tempo exec.: %fs\n", diff);
+
 	free(thread);
 	free(thread_params);
 	
 	showArray(v, arraySize);
-	
 	free(v);
+
 	return 0;
 }
-
 
 void* task(void *arg){
 	t_params* params = (t_params *) arg;
@@ -83,8 +101,12 @@ void* task(void *arg){
 	if(params->remainingIndexOffset != -1)
 		++v[arraySize-params->remainingIndexOffset-1];
 
-	pthread_exit(NULL);
+	return NULL;
 }
+
+
+
+
 
 void showArray(int *v, int size){
 	for(int i = 0; i < size; ++i)
